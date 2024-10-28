6b094ed0f488319b36d4ea088bbf3a267545db0f
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-28 12:57:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more. ";
+            string s2 = "Italija je prvak svijeta";
+            
+
+            Console.WriteLine(s1.Replace ("ima more.", s2));
+
+            Console.ReadKey();
+        
+        }
+    }
+}
